5a3df7b43774d983591ecdb6b07431eed9bd06c4
nomidi <mail@bastian-fritsch.de>
2023-09-05 11:24:40 +0200

    Update PageControllerExtension.php

--- src/Controllers/PageControllerExtension.php
+++ src/Controllers/PageControllerExtension.php
@@ -5,6 +5,7 @@ namespace kw\searchhero;
 
 use Page;
 use SilverStripe\Core\Extension;
+use SilverStripe\Dev\Debug;
 use SilverStripe\Forms\FieldList;
 use SilverStripe\Forms\Form;
 use SilverStripe\Forms\FormAction;
@@ -18,11 +19,16 @@ class PageControllerExtension extends Extension
 
     public function SearchHeroForm()
     {
+        if (isset($_POST['action_doSearch'])) {
+            $searchvalue = $_POST['Title'];
+        } else {
+            $searchvalue = "";
+        }
         $fields = new FieldList(
-            new TextField('Title',_t("SearchHero.SearchField","Search"))
+            new TextField('Title', _t("SearchHero.SearchField", "Search"), $searchvalue)
         );
         $actions = new FieldList(
-            new FormAction('doSearch', _t("SearchHero.Submit","Submit"))
+            new FormAction('doSearch', _t("SearchHero.Submit", "Submit"))
         );
 
         return new Form($this->owner, 'SearchHeroForm', $fields, $actions);
@@ -35,13 +41,10 @@ class PageControllerExtension extends Extension
         return $this->owner->customise([
             'Layout' => $this->owner
                 ->customise([
-                             'Query' => $data['Title'],
+                    'Query' => $data['Title'],
                     'Results' => $results,
                 ])
                 ->renderWith(['kw\searchhero\SearchHeroForm_results']),
         ])->renderWith(['Page']);
     }
 }
-
-
-
